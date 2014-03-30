caf79f167152d2eb2b8b81697bf98a6eb4580d0c
Ortin <Ortin@10.80.2.31>
2014-03-30 16:35:15 -0600

    Finished pursuing dead end, cleaned up dead end code.
    Attempts to post the group from JS failed.  The problem arose that I couldn't authenticate the group request from the JS page because I didn't have the app secret, but practice forbids me from using the app secret client side due to security concerns.  An alternate solution presents itself in the form of server side HTTP requests to the facebook api, which can be used to create the group.  The request has to be made manually, but I was already trying to do that client side.

--- web/hunt.js
+++ web/hunt.js
@@ -5,6 +5,64 @@
  */
 
 var appid = "480920812033752"; //Grabbed this from the Facebook API app page
+
+            window.fbAsyncInit = function() {
+            FB.init({
+                appId      : appid,
+                status     : true, // check login status
+                cookie     : true, // enable cookies to allow the server to access the session
+                xfbml      : true  // parse XFBML
+            });
+
+            // Here we subscribe to the auth.authResponseChange JavaScript event. This event is fired
+            // for any authentication related change, such as login, logout or session refresh. This means that
+            // whenever someone who was previously logged out tries to log in again, the correct case below 
+            // will be handled. 
+            FB.Event.subscribe('auth.authResponseChange', function(response) {
+                // Here we specify what we do with the response anytime this event occurs. 
+                if (response.status === 'connected') {
+                    // The response object is returned with a status field that lets the app know the current
+                    // login status of the person. In this case, we're handling the situation where they 
+                    // have logged in to the app.
+                    testAPI();
+                } else if (response.status === 'not_authorized') {
+                    // In this case, the person is logged into Facebook, but not into the app, so we call
+                    // FB.login() to prompt them to do so. 
+                    // In real-life usage, you wouldn't want to immediately prompt someone to login 
+                    // like this, for two reasons:
+                    // (1) JavaScript created popup windows are blocked by most browsers unless they 
+                    // result from direct interaction from people using the app (such as a mouse click)
+                    // (2) it is a bad experience to be continually prompted to login upon page load.
+                    FB.login();
+                } else {
+                    // In this case, the person is not logged into Facebook, so we call the login() 
+                    // function to prompt them to do so. Note that at this stage there is no indication
+                    // of whether they are logged into the app. If they aren't then they'll see the Login
+                    // dialog right after they log in to Facebook. 
+                    // The same caveats as above apply to the FB.login() call here.
+                    FB.login();
+                }
+            });
+        };
+
+        // Load the SDK asynchronously
+        (function(d){
+            var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+            if (d.getElementById(id)) {return;}
+            js = d.createElement('script'); js.id = id; js.async = true;
+            js.src = "//connect.facebook.net/en_US/all.js";
+            ref.parentNode.insertBefore(js, ref);
+        }(document));
+
+        // Here we run a very simple test of the Graph API after login is successful. 
+        // This testAPI() function is only called in those cases. 
+        function testAPI() {
+            console.log('Welcome!  Fetching your information.... ');
+            FB.api('/me', function(response) {
+            console.log('Good to see you, ' + response.name + '.');
+            });
+        }
+
 /**
  * Generates the number of text fields specified by Number of Clues field
  * and assigns them ids for future use.
@@ -61,28 +119,6 @@ function postGroup(){
         window.alert("Both the group name field and the group description fields must be filled.");
         return;
     }
-
-    /* make the API call */
-    /*
-    FB.api(
-        "/" + appid + "/groups",
-        "POST",
-        {
-            "object": {
-                "name"          : name,
-                "description"   : groupDesc
-            }
-        },
-        function (response) {
-            if (response && !response.error) {
-                // handle the result
-            }
-            else {
-                window.alert("Facebook creation failed with error: " + response.error);
-            }
-        }
-    ); */
-      
     document.getElementById("p1").style.visibility="hidden";
     document.getElementById("p2").style.display="none";
     document.getElementById("p3").style.visibility="visible";
